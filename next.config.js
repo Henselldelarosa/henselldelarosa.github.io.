4ec9e3a1996a1b389aa448b84075fd9dc2f653bc
Hensell De La Rosa <97318548+Henselldelarosa@users.noreply.github.com>
2026-02-22 18:53:47 -0500

    Update next.config.js
    update config

--- next.config.js
+++ next.config.js
@@ -2,27 +2,28 @@ const withPlugins = require("next-compose-plugins");
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
+const repo = "my-portfolio"; // <-- IMPORTANT: your repo name
+
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        deviceSizes: [640, 768, 1024, 1280, 1536],
-        loader: "custom",
-        path: "/",
-    },
+  reactStrictMode: true,
+  output: "export",
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
+  images: { unoptimized: true },
 };
 
 module.exports = withPlugins(
+  [
     [
-        [
-            withPWA,
-            {
-                pwa: {
-                    disable: process.env.NODE_ENV === "development",
-                    dest: "public",
-                    runtimeCaching,
-                },
-            },
-        ],
+      withPWA,
+      {
+        pwa: {
+          disable: process.env.NODE_ENV === "development",
+          dest: "public",
+          runtimeCaching,
+        },
+      },
     ],
-    nextConfig
+  ],
+  nextConfig
 );
